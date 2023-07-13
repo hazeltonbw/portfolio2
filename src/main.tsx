5e51cdf289849cc54f72ee3d51fbdcfaa712647d
Brandon Hazelton <hazeltonbw@gmail.com>
2023-07-12 18:33:57 -0700

    eslint formatting

--- src/main.tsx
+++ src/main.tsx
@@ -1,39 +1,39 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import Root from "./pages/Root.tsx";
-import About from "./pages/About.tsx";
-import Home from "./pages/Home.tsx";
-import Contact from "./pages/Contact.tsx";
-import Projects from "./pages/Projects.tsx";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Root from './pages/Root.tsx'
+import About from './pages/About.tsx'
+import Home from './pages/Home.tsx'
+import Contact from './pages/Contact.tsx'
+import Projects from './pages/Projects.tsx'
 
 const router = createBrowserRouter([
   {
-    path: "/",
+    path: '/',
     element: <Root />,
     children: [
       {
-        path: "/",
+        path: '/',
         element: <Home />,
       },
       {
-        path: "/about",
+        path: '/about',
         element: <About />,
       },
       {
-        path: "/contact",
+        path: '/contact',
         element: <Contact />,
       },
       {
-        path: "/projects",
+        path: '/projects',
         element: <Projects />,
       },
     ],
   },
-]);
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+])
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <RouterProvider router={router} />
   </React.StrictMode>
-);
+)
