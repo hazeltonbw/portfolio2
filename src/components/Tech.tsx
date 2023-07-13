665cf54fbf2125d63306b249df73bd077f1b1dea
Brandon Hazelton <hazeltonbw@gmail.com>
2023-07-12 18:37:04 -0700

    Add styles and have TechUsed render children

--- src/components/Tech.tsx
+++ src/components/Tech.tsx
@@ -1,13 +1,16 @@
-import React from "react";
+import React from 'react'
 
-type Props = { children: any };
+type Props = { children: any }
 
 const Tech = (props: Props) => {
   return (
-    <li className="flex flex-col items-center justify-center text-center">
+    <li
+      className='flex flex-col gap-y-1 justify-start  items-center text-center 
+    dark:fill-sky-500 text-sky-900 dark:text-sky-100'
+    >
       {props.children}
     </li>
-  );
-};
+  )
+}
 
-export default Tech;
+export default Tech
--- src/components/TechUsed.tsx
+++ src/components/TechUsed.tsx
@@ -1,73 +1,16 @@
-import { IconContext } from "react-icons";
-import { FaReact, FaNodeJs, FaCcStripe, FaHtml5 } from "react-icons/fa";
-import {
-  SiRedux,
-  SiReactrouter,
-  SiPostgresql,
-  SiExpress,
-  SiPassport,
-  SiJest,
-  SiTypescript,
-  SiTailwindcss,
-} from "react-icons/si";
-import Tech from "./Tech";
+import { IconContext } from 'react-icons'
+type Props = {
+  children: any
+}
 
-const TechUsed = () => {
+const TechUsed = (props: Props) => {
   return (
-    <ul className="grid gap-4 m-2 grid-cols-3 content-center ">
-      <IconContext.Provider value={{ size: "32" }}>
-        <Tech>
-          <FaReact />
-          <h3>React</h3>
-        </Tech>
-        <Tech>
-          <SiRedux />
-          <h3>Redux</h3>
-        </Tech>
-        <Tech>
-          <SiReactrouter />
-          <h3>React Router</h3>
-        </Tech>
-        <Tech>
-          <SiPostgresql />
-          PostgreSQL
-        </Tech>
-        <Tech>
-          <SiExpress />
-          Express
-        </Tech>
-        <Tech>
-          <FaNodeJs />
-          Node
-        </Tech>
-        <Tech>
-          <SiPassport />
-          Passport
-        </Tech>
-        <Tech>
-          <FaCcStripe />
-          Stripe
-        </Tech>
-        <Tech>
-          <SiJest />
-          Jest
-        </Tech>
-
-        <Tech>
-          <SiTypescript />
-          TypeScript
-        </Tech>
-        <Tech>
-          <FaHtml5 />
-          HTML
-        </Tech>
-        <Tech>
-          <SiTailwindcss />
-          TailwindCSS
-        </Tech>
+    <ul className='grid gap-y-4 gap-x-1 md:gap-x-0 my-4 grid-cols-2  sm:grid-cols-4 lg:grid-cols-4'>
+      <IconContext.Provider value={{ size: '32' }}>
+        {props.children}
       </IconContext.Provider>
     </ul>
-  );
-};
+  )
+}
 
-export default TechUsed;
+export default TechUsed
