eebe708f0d4c047aecf2b89b7b22f1579c7c1fca
type <type>
2023-07-30 00:12:35 -0700

    Start about me page

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,9 +1,26 @@
+import svg from '../assets/hmm.svg'
 const About = () => {
   return (
-    <section className="flex flex-1">
-      <h1>About</h1>
+    <section className='flex flex-1 flex-col items-center '>
+      <div className='px-8 mb-16'>
+        <h1 className='my-8'>About Me</h1>
+        <p className='text-lg '>
+          I'm an enthusiastic and driven full-stack developer with a passion for
+          crafting innovative and user-friendly digital solutions. With a solid
+          foundation in both front-end and back-end technologies, I thrive on
+          turning ideas into reality through clean and efficient code. My
+          journey in the world of programming began with a curiosity for web
+          development, which led me to explore HTML, CSS, and JavaScript. I am
+          constantly honing my skills, eager to embrace new technologies and
+          best practices to create seamless and impactful applications. My
+          ultimate goal is to contribute my creativity and problem-solving
+          skills to a dynamic and collaborative team, all while continuously
+          evolving as a professional in the ever-evolving landscape of software
+          development.
+        </p>
+      </div>
     </section>
-  );
-};
+  )
+}
 
-export default About;
+export default About
